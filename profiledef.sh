b3275849341179f481517e91f8e5205325ddec99
Saurabh Ahuja <sauraahu@gmail.com>
2024-11-10 13:46:09 +0530

    add xorg

--- profiledef.sh
+++ profiledef.sh
@@ -12,7 +12,7 @@ bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
            'uefi-ia32.systemd-boot.esp' 'uefi-x64.systemd-boot.esp'
            'uefi-ia32.systemd-boot.eltorito' 'uefi-x64.systemd-boot.eltorito')
 arch="x86_64"
-pacman_conf="pacman.conf"
+pacman_conf="airootfs/etc/pacman.conf"
 airootfs_image_type="squashfs"
 airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
 bootstrap_tarball_compression=('zstd' '-c' '-T0' '--auto-threads=logical' '--long' '-19')
